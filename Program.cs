c69c03f4e09be5eb14502435e43992e10ca1e4ce
Velin Arbov <arbov.v@gmail.com>
2021-02-24 16:27:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,18 +7,11 @@ namespace MarketStore
         static void Main(string[] args)
         {
 
-            
-
-
-
             var owner2 = new Owner("Georgi", "Georgiev");
             var bronzeCard = new Card(owner2, 301, 100, "Bronze");
 
             var owner1 = new Owner("Velin", "Arbov");
             var silverCard = new Card(owner1, 301, 850, "Silver");
-
-
-
 
             var owner3 = new Owner("Ivan", "Georgiev");
             var goldCard = new Card(owner3, 300, 1300, "Gold");
